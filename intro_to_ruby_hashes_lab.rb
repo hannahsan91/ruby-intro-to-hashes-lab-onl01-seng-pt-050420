cedc2ad8d764cfa6ca1d18d105e188d26d0c8ee3
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-15 00:17:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,45 +1,44 @@
-require 'pry'
 def new_hash
-  new_hash = {}
+	new_hash = {}
 end
 
 def actor
-  actor = {name: "Dwayne The Rock Johnson"}
+	actor = {name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-  binding.pry
-  monopoly = {
+	monopoly = {
 		:railroads => {}
 	}
+
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-  monopoly = {
+	monopoly = {
 		:railroads => {pieces: 4, names: {}, rent_in_dollars: {}}
 	}
+
 end
 
 def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-  monopoly = {
-  		:railroads => {
-  			pieces: 4,
-  			names: {
-  				reading_railroad: {},
-  				pennsylvania_railroad: {},
-  				b_and_o_railroad: {},
-  				shortline: {}
-  			},
-  			rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
-  		}
-  	}
+
+	monopoly = {
+		:railroads => {
+			pieces: 4,
+			names: {
+				reading_railroad: {},
+				pennsylvania_railroad: {},
+				b_and_o_railroad: {},
+				shortline: {}
+			},
+			rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
+		}
+	}
+
+
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-  def monopoly_with_fourth_tier
 	monopoly = {
 		:railroads => {
 			pieces: 4,
@@ -52,4 +51,5 @@ def monopoly_with_fourth_tier
 			rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
 		}
 	}
+
 end
